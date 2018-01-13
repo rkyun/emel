359bb799d73f5212e5b9c81153f0342b6ae73ffb
rykun <rykunek@hotmail.com>
2018-01-13 23:26:36 +0100

    react-native link realm

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ListView from './ListView';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>App</Text>
+        <ListView></ListView>
       </View>
     );
   }
